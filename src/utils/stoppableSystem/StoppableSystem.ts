5c7294a832aa463f9995e0ebc4d0ab52263293ca
Logan Earl <learl@commerce-architects.com>
2023-01-17 16:31:35 -0800

    Simplified stoppable systems

--- src/utils/stoppableSystem/StoppableSystem.ts
+++ src/utils/stoppableSystem/StoppableSystem.ts
@@ -1,9 +1,14 @@
-abstract class StoppableSystem<V> {
-    abstract loadMemory(): StoppableMemory<V>
-    abstract saveMemory(memory: StoppableMemory<V>): void
+import { type } from "os";
 
-    public addStopReason(reason: V){
-        let memory = this.loadMemory()
-        if(!memory.)
+abstract class StoppableSystem<V> {
+    abstract loadMemory(): StoppableMemory<V>;
+    abstract saveMemory(memory: StoppableMemory<V>): void;
+
+    public addStopReason(reason: V) {
+        let memory = this.loadMemory();
+        if (!memory.stopReasons) memory.stopReasons = [reason];
+        else memory.stopReasons.push(reason);
+
+        if (memory.state !== "Stopped") memory.state = "Stopped";
     }
 }
--- src/utils/stoppableSystem/index.d.ts
+++ src/utils/stoppableSystem/index.d.ts
@@ -1,14 +1,6 @@
 type StoppableSystemState = "New" | "Running" | "Stopped";
 
-interface StateChangeEvent<V> {
-    prevState: StoppableSystemState;
-    newState: StoppableSystemState;
-    addedStopReason?: V;
-    removedStopReason?: V;
-}
-
 interface StoppableMemory<V> {
     state: StoppableSystemState;
     stopReasons: V[];
-    stateHistory: StateChangeEvent<V>;
 }
